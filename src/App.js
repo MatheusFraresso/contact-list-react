18d8be2a2283874e5c08bb5f1bd15d3a7a49255b
MatheusFraresso <matheusmfraresso@gmail.com>
2022-04-10 20:35:25 -0300

    First code commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import UsersList from "./components/UsersList";
+import CreateUserForm from "./components/forms/CreateUserForm";
+
+import api from "./api";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  async function fetchUsers() {
+    const updatedUsersList = (await api.get("/users")).data;
+    setUsers(updatedUsersList);
+  }
+  const updateList = async () => {
+    fetchUsers();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "1200px" }}>
+      <div style={{ width: "400px", float: "left" }}>
+        <UsersList users={users} updateList={updateList} />
+      </div>
+
+      <div style={{ width: "400px", float: "left" }}>
+        <CreateUserForm actiontype="new" userCreated={updateList} />
+      </div>
     </div>
   );
 }
